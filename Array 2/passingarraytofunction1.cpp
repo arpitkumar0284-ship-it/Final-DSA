c32ace4735486fcc4646842a5d48bdf8107ceaf9
ArpitKumar <arpitkumar0284@gmail.com>
2026-08-02 20:36:00 +0530

    Understand the concept of passing array to func

---
+++ Array 2/passingarraytofunction1.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+void display(int a[],int size)
+{
+  for(int i=0;i<size;i++)   
+  {
+    cout<<a[i]<<" ";
+  }
+  cout<<endl;
+  return;
+}
+void change(int b[],int size)
+{
+    b[0]=100;
+}
+int main()
+{
+    int arr[5]={1,4,2,7,4};
+    int size=sizeof(arr)/sizeof(arr[0]);
+    //accessing the elements of array in another function
+    // updation
+    display(arr,size);
+    change(arr,size);
+    display(arr,size);
+}
