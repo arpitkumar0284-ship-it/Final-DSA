1fa26a2d90aa4a8c639ab3b146728aceb2e2f0e7
ArpitKumar <arpitkumar0284@gmail.com>
2026-08-03 19:41:16 +0530

    Add rev array code using two pointer

---
+++ Array 2/ReverseVectorInPlace14.cpp
@@ -0,0 +1,34 @@
+ #include<iostream>
+ #include<vector>
+ using namespace std;
+ void display(vector<int>&a)
+ {
+    for(int i=0;i<a.size();i++)
+    {
+        cout<<a[i]<<" ";
+    }
+    cout<<endl;
+ }
+ int main()
+ {
+   vector<int> v;
+   v.push_back(1);
+   v.push_back(9);
+   v.push_back(7);
+   v.push_back(6);
+   v.push_back(4);
+   v.push_back(2);
+   v.push_back(5);
+
+   display(v);
+   //reverse using two pointer techniques
+   int i=0;
+   int j=v.size()-1;
+   while(i<j)
+   {
+     swap(v[i],v[j]);
+     i++;
+     j--;
+   }
+   display(v);
+ }
