d2fe34f6ff345c5804bed22044f32433505d7c80
ArpitKumar <arpitkumar0284@gmail.com>
2026-08-04 17:56:03 +0530

    Add 2D array sum code

---
+++ 2D Arrays 1/2DArraySum5.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   int m;
+   cout<<"Enter the no. of rows:";
+   cin>>m;
+   int n;
+   cout<<"Enter the no. of colums:";
+   cin>>n;
+   int arr[m][n];
+   cout<<"Enter the elements:";
+   for(int i=0;i<m;i++)
+   {
+    for(int j=0;j<n;j++)
+    {
+        cin>>arr[i][j];
+    }
+   }
+    int sum=0;
+    for(int i=0;i<m;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            sum+=arr[i][j];
+        }
+
+    }
+    cout<<sum;
+}
