bd76ea6aa74d2bd5280467d5ded316d241528fb6
ArpitKumar <arpitkumar0284@gmail.com>
2026-08-05 16:30:54 +0530

    Add the code of concept behind paasing 2D array into functions

---
+++ 2D Arrays 3/2DArrayintoFunctions2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+void change2D(int arr[3][3]){
+    arr[0][0]=100;
+}
+int main()
+{
+    int arr[3][3]={{1,2,3},{4,5},{7,8,9}};
+    cout<<arr[0][0]<<endl;
+    change2D(arr);
+    cout<<arr[0][0]<<endl;
+    
+}
