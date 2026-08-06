058f95051e12bdf7d4f34e6418892c5c1187d5ac
ArpitKumar <arpitkumar0284@gmail.com>
2026-08-06 14:31:29 +0530

    Add some basic concepts of string program

---
+++ Strings 1/introStrings1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    char str[5]= "abcd";
+    // char str[5]={'a','b','c','d'};
+    // for(int i=0;i<5;i++)
+    // {
+    //     cout<<str[i]<<" ";
+    // }
+    cout<<str<<endl;
+}
