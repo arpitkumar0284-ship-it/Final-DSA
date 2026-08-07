c72bbb2ea70cfbc76518d44ad1d1427a90e91d8a
ArpitKumar <arpitkumar0284@gmail.com>
2026-08-07 23:43:00 +0530

    Add code of basic operation of vector

---
+++ Array 2/operationofvector4.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+    vector<int> v;
+    v.push_back(1);
+    v.push_back(9);
+    v.push_back(7);
+    v.push_back(8);
+    v.push_back(54);
+    v.push_back(81);
+    v.push_back(65);
+    v.push_back(32);
+    v.push_back(79);
+    v.push_back(35);
+    for(int i=0;i<v.size();i++)
+    {
+        cout<<v[i]<<" ";
+    }
+    v.pop_back();
+    v.pop_back();
+    cout<<endl;
+     for(int i=0;i<v.size();i++)
+    {
+        cout<<v[i]<<" ";
+    }
+    
+}
