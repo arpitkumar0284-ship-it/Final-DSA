bf7ccb4af74a20fafb67a819e52f918d64cc9d1e
ArpitKumar <arpitkumar0284@gmail.com>
2026-08-09 23:26:28 +0530

    Add sixth program of cpp basics

---
+++ C++ Basics/GreetingFunction6.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+void greeting()
+{
+    cout<<"Good maorning"<<"\n";
+    cout<<"Have a nice day"<<"\n";
+}
+int main()
+{
+    greeting(); //function calling
+}
