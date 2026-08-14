2ffd5a2201bf522fabb11de05519cb4c3763aaec
ArpitKumar <arpitkumar0284@gmail.com>
2026-08-14 14:35:30 +0530

    Add a basic program of the string and the string is taken as input by the user, using getline function

---
+++ C++ Basics/StringInputUsingGetline22.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   //string str = "Arpit Kumar";
+  // cout<<str;
+  string s;
+ // cin>>s;  ->ONLY IF THE GIVEN STRING HAS NO SPACES
+  getline(cin,s);  //VVIMP
+  cout<<s;
+}
