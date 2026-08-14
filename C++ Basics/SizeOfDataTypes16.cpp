14fbaeeb8e71610e855e4a2774a4c9c391844ab7
ArpitKumar <arpitkumar0284@gmail.com>
2026-08-14 14:13:16 +0530

    Add a program to print the size of different data types

---
+++ C++ Basics/SizeOfDataTypes16.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<sizeof(int)<<"\n";
+    cout<<sizeof(char)<<"\n";
+    cout<<sizeof(long long)<<"\n";
+    cout<<sizeof(float)<<"\n";
+    cout<<sizeof(bool)<<"\n";
+    cout<<sizeof(double)<<"\n";
+}
