164c62ca0c1731d86ec952a3e44b035b27de5691
ArpitKumar <arpitkumar0284@gmail.com>
2026-08-15 11:59:05 +0530

    Got a concept of return in a function

---
+++ Recursion 1/FunctionCall12.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+void gun()
+{
+    return;  //khatam
+    cout<<"Hello CW"<<endl;
+}
+void fun()
+{
+    cout<<"Hello PW"<<endl;
+    gun();
+    return;
+}
+int main()
+{
+    fun();
+}
