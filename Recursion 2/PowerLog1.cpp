85b69faf75d87d346528a009bf0d66cdea083144
ArpitKumar <arpitkumar0284@gmail.com>
2026-08-16 11:09:13 +0530

    Add a program to find out the power of a given number using logarithm

---
+++ Recursion 2/PowerLog1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int pow(int x,int n)
+{
+    if(n==1) return x;
+    if(n%2==0) return pow(x,n/2)*pow(x,n/2);
+    if(n%2!=0) return pow(x,n/2)*pow(x,n/2)*x;
+}
+
+int main()
+{
+    cout<<pow(5,3);
+}
