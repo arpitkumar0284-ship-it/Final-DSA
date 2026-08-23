03c98c8645dba5216de4d4545bf40210cb4cf116
ArpitKumar <arpitkumar0284@gmail.com>
2026-08-23 14:21:22 +0530

    Add a program to find the gcd of two numbers without using recursion

---
+++ Recursion 4/GCD_without_recursion4.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int hcf(int a, int b)
+{
+    for(int i=min(a,b);i>=2;i--)
+    {
+        if(a%i==0 && b%i==0) return i;
+    }
+    return 1;
+}
+int main()
+{
+    int a=27;
+    int b=45;
+    cout<<hcf(a,b);
+}
