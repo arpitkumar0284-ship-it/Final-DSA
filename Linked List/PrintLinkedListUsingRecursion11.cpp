57203a60c7974525f9ff5706107b496902ab27fa
ArpitKumar <arpitkumar0284@gmail.com>
2026-09-08 20:56:53 +0530

    Add a program to print the values of linked list using recursion

---
+++ Linked List/PrintLinkedListUsingRecursion11.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+class Node{   //Linked list Node
+public:
+   int val;
+   Node* next;
+   Node(int val)
+   {
+       this->val=val;
+       this->next=NULL;
+   }
+};
+
+void displayRec(Node* head)
+{
+    if(head==NULL) return;
+    cout<<head->val<<" ";
+    displayRec(head->next);
+}
+int main()
+{   
+    Node* a =new Node(10);
+    Node* b =new Node(20);
+    Node* c =new Node(30);
+    Node* d =new Node(40);
+    Node* e =new Node(50);
+    a->next=b;
+    b->next=c;
+    c->next=d;
+    d->next=e;
+   displayRec(a);
+}
